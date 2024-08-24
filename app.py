de50b57446e087f99d799471fd0957833e8088e8
Daniyar Agaltsov <agaltsovdaniyar@gmail.com>
2024-08-25 01:13:42 +0800

    Changed something

--- app.py
+++ app.py
@@ -363,5 +363,6 @@ def create_user():
 
 # ==================Main=======================
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
